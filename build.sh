d2db593adbd55e4894d17480ccf7bff0586f3aa4
smilexth <admin@jack.in.th>
2023-08-11 17:23:10 +0700

    add: push

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 # REMOVE EXISTING IMAGE
-docker rmi -f smilexth/cloudflare-ddns:latest  ;
-docker rmi -f smilexth/cloudflare-ddns:amd64  ;
-docker rmi -f smilexth/cloudflare-ddns:amd64-$1  ;
-docker rmi -f smilexth/cloudflare-ddns:arm64v8 ;
-docker rmi -f smilexth/cloudflare-ddns:arm64v8-$1  ;
+docker rmi smilexth/cloudflare-ddns:latest  ;
+docker rmi smilexth/cloudflare-ddns:amd64  ;
+docker rmi smilexth/cloudflare-ddns:amd64-$1  ;
+docker rmi smilexth/cloudflare-ddns:arm64v8 ;
+docker rmi smilexth/cloudflare-ddns:arm64v8-$1  ;
 
+
+# LATEST (AMD64)
+docker build -t smilexth/cloudflare-ddns:latest -f ./Dockerfile-amd64 . ;
 
 # ARM64
 docker build -t smilexth/cloudflare-ddns:arm64v8 -f ./Dockerfile-arm64v8 . ;
@@ -16,5 +19,10 @@ docker build -t smilexth/cloudflare-ddns:arm64v8-$2 -f ./Dockerfile-arm64v8 . ;
 docker build -t smilexth/cloudflare-ddns:amd64 -f ./Dockerfile-amd64 . ;
 docker build -t smilexth/cloudflare-ddns:amd64-$2 -f ./Dockerfile-amd64 . ;
 
-# LATEST (AMD64)
-docker build -t smilexth/cloudflare-ddns:latest -f ./Dockerfile-amd64 . ;
+
+# PUSH ALL
+docker push smilexth/cloudflare-ddns:latest
+docker push smilexth/cloudflare-ddns:arm64v8
+docker push smilexth/cloudflare-ddns:arm64v8-$2
+docker push smilexth/cloudflare-ddns:amd64
+docker push smilexth/cloudflare-ddns:amd64-$2
